5095f1691cfb9157487bce66b845d4225f00bb6e
Bogdan Ghita <bogdan.ghita93@gmail.com>
2019-02-27 00:03:54 +0100

    add original queries

---
+++ benchmark/Corporations/queries-original/queries.sql
@@ -0,0 +1,2 @@
+SELECT "Corporations_1"."company_name" AS "company_name" FROM "Corporations_1" WHERE ("Corporations_1"."company_name" IS NULL) LIMIT 1
+SELECT "Corporations_1"."name" AS "name" FROM "Corporations_1" WHERE ("Corporations_1"."name" IS NULL) LIMIT 1
